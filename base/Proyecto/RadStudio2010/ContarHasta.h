416ed4006f7484c357fb7949e87826ddafdece84
Torrentarium <torrentelinux@gmail.com>
2019-04-14 18:22:37 -0300

    Add files via upload

---
+++ base/Proyecto/RadStudio2010/ContarHasta.h
@@ -0,0 +1,24 @@
+// ContarHasta.h
+// Octulio Biletán - torrentelinux@gmail.com - 04|2019
+// Realizar la cuenta desde 0 hasta 2^N - 1
+// Proyecto ContarHasta.cbproj para Embarcadero RAD Studio 10.2/RAD Studio 2010
+
+#pragma hdrstop
+#pragma argsused
+
+#ifdef _WIN32
+#include <tchar.h>
+#else
+  typedef char _TCHAR;
+  #define _tmain main
+#endif
+
+#include <windows.h>
+#include <dos.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
