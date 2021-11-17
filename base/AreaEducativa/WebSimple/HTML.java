7af1ef6bdafbeb509e0acc547eb9fd53d41f5fe8
Torrentarium <torrentelinux@gmail.com>
2021-11-17 14:37:07 -0300

    Add files via upload

--- base/AreaEducativa/WebSimple/HTML.java
+++ base/AreaEducativa/WebSimple/HTML.java
@@ -42,7 +42,7 @@ import javax.servlet.http.HttpServletResponse;
 public final class HTML
 {
     private PrintWriter hoja;
-    
+
     HTML(HttpServletResponse r) throws IOException
     {
         hoja = r.getWriter();
@@ -53,7 +53,7 @@ public final class HTML
         // La codificación del texto es UTF-8
         r.setCharacterEncoding("UTF-8");
     }
-    
+
     void openHtml()
     {
         // Pone la declaración del tipo de documento (DTD)
@@ -61,20 +61,20 @@ public final class HTML
         
         // Empieza la página HTML
         hoja.println("<html>");
-    }   
-    
+    }
+
     void closeHtml()
-    {        
+    {
         // Termina la página HTML
         hoja.println("</html>");
     }
-    
+
     void openHead()
-    {        
+    {
         hoja.println("<head>");
         hoja.println("<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">");
     }
-    
+
     void closeHead()
     {
         hoja.println("</head>");
@@ -83,13 +83,13 @@ public final class HTML
     void openBody()
     {
         hoja.println("<body>");
-    }        
-            
+    }
+
     void closeBody()
     {
         hoja.println("</body>");
-    }        
-    
+    }
+
     // Identifica el generador de la página html a través de la etiqueta 'meta'
     // Usar dentro del bloque '<head>'
     void setGenerator(String g)
@@ -98,7 +98,7 @@ public final class HTML
         hoja.print(g);
         hoja.println("\">");
     }
-    
+
     // Identifica el autor de la página html a través de la etiqueta 'meta'
     // Usar dentro del bloque '<head>'
     void setAuthor(String a)
@@ -121,37 +121,75 @@ public final class HTML
     {
         hoja.println("<title>" + t + "</title>");
     }
-    
+
+    // Pone color de fondo 'bg' y color de frente 'fg' sobre el texto 't'
+    void putTextColor(String bg, String fg, String t)
+    {
+        hoja.print("<span style=\"background-color: ");
+        hoja.print(bg);
+        hoja.print("; color: ");
+        hoja.print(fg);
+        hoja.print(";\">");
+        hoja.print(t);
+        hoja.print("</span>");
+    }
+            
+    void putTextBr(String t)
+    {
+        if(t.isEmpty())
+          hoja.println("<br>");
+        else
+          hoja.println(t + "<br>");
+    }
+
     void putText(String t)
     {
-        hoja.println(t + "<br>");
+        hoja.print(t);
     }
 
     // El texto 'c' va acompañado del atributo 'align=center'.
     // Ajuste del texto centrado en la hoja.
     String alignCenter(String c)
-    {        
+    {
         return "align=center^" + c;
     }
-    
+
     // Pone texto 't' a 1º Nivel de encabezamento (Heading level 1)
     // --Título muy grande--
     void putTitle1(String t)
     {
         int pos;
-        
+
         hoja.print("<h1");
         pos = t.indexOf('^');
         if(pos != -1)
-        {    
+        {
           hoja.print(" " + t.substring(0, pos) + ">");
           hoja.print(t.substring(pos+1));
           hoja.println("</h1><br>");
         }
         else
-        {    
+        {
             hoja.print(">" + t);
             hoja.println("</h1><br>");
-        }    
+        }
     }
-}   
+    
+    void putHorizontalLine(String hl)
+    {
+        Integer valor = new Integer(hl);
+        String ancho;
+        
+        if(hl.isEmpty())
+            ancho = "100";
+        else
+            ancho = hl;
+        
+        if(valor < 0 || valor > 100)
+            ancho = "100";
+        
+        hoja.print("<hr width=\"");
+        hoja.print(ancho);
+        hoja.print("%\" size=\"1\">");
+    }
+}
