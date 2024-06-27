5b617da07b0bf81b54feb89764f13e4898d74ce0
Torrentarium <torrentelinux@gmail.com>
2024-06-27 17:13:35 -0300

    Add files via upload

--- base/programa/miip.cmd
+++ base/programa/miip.cmd
@@ -1,19 +1,20 @@
 @echo off
-REM Eugenio Martínez - https://t.me/octulioBiletan - Abril de 2023-
-REM Versión 2.0 de miip.cmd para Windows.
-REM Muestra la dir. IP local y externa de su PC conectada a internet.
+REM Eugenio Martínez - https://t.me/octulioBiletan - Junio de 2024-
+REM Versión 3.0 de miip.cmd para Windows.
+REM Muestra la dir. IP local (v4 y v6) y externa de su PC conectada a internet.
 REM Es necesario instalar curl.exe
 REM -----------------------------------------------------------------
 REM AVISO:
 REM Instale curl.exe en su sistema.
 title Usuario y dir. IP activa local y externa
-mode con cols=78 lines=32
+mode con cols=84 lines=36
 echo [Usuario activo]
 echo %USERNAME%
 whoami /user
 echo.
 echo [Config. IP - Local]
 ipconfig /all|find /i "ipv4"
+ipconfig /all|find /i "ipv6"
 echo.
 echo [Config. IP - Externa]
 curl -s https://ipinfo.io/json
