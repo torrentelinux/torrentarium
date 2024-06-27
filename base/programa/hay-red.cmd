4d05a8af0a6b8e0766fd00d6aedabdfa1d743de8
Torrentarium <torrentelinux@gmail.com>
2024-06-27 17:28:12 -0300

    Update hay-red.cmd

--- base/programa/hay-red.cmd
+++ base/programa/hay-red.cmd
@@ -1,11 +1,11 @@
 @echo off
-REM Eugenio MartÌnez - https://t.me/octulioBiletan - Junio de 2024-
-REM VersiÛn 8.0 de hay-red.cmd para Windows en espaÒol y en inglÈs.
-REM Comprueba el funcionamiento b·sico del servicio DNS.
-REM Es m·s eficaz que las otras versiones anteriores.
+REM Eugenio Mart√≠nez - https://t.me/octulioBiletan - Junio de 2024-
+REM Versi√≥n 8.0 de hay-red.cmd para Windows en espa√±ol y en ingl√©s.
+REM Comprueba el funcionamiento b√°sico del servicio DNS.
+REM Es m√°s eficaz que las otras versiones anteriores.
 REM Uso: hay-red [/a]|[/c]|[/s]
 REM /a Muestra una simple ayuda del comando.
-REM /c Muestra una ayuda m·s completa del comando.
+REM /c Muestra una ayuda m√°s completa del comando.
 REM /s Corre el comando en modo silencioso, no muestra demasiado texto en pantalla.
 REM -------------------------------------------------------------------------------
 chcp 1252 > nul
@@ -13,11 +13,11 @@ set silencioso=0
 set txtAyuda=%~dp0\hay-red-ayuda.txt
 
 if "%1" == "" goto paso1
-if "%1" == "/a" (echo [Ayuda]&&echo OpciÛn /c --- muestra una ayuda m·s completa&&echo OpciÛn /s --- modo silencioso&&goto fin)
+if "%1" == "/a" (echo [Ayuda]&&echo Opci√≥n /c --- muestra una ayuda m√°s completa&&echo Opci√≥n /s --- modo silencioso&&goto fin)
 if "%1" == "/c" (type %txtAyuda%&&goto fin)
 if "%1" == "/s" (set silencioso=1&&goto paso1)
-echo ERROR: OpciÛn no reconocida.
-echo Solicite una ayuda con opciÛn /a.&&goto fin
+echo ERROR: Opci√≥n no reconocida.
+echo Solicite una ayuda con opci√≥n /a.&&goto fin
 
 :paso1
 ver /?|find "Display" > nul
@@ -27,36 +27,36 @@ ver /?|find "Muestra" > nul
 if %ERRORLEVEL% == 0 goto Wcastellano
 ver /?|find "Mostrar" > nul
 if %ERRORLEVEL% == 0 goto Wcastellano
-echo Idioma no reconocido: espaÒol e inglÈs ˙nicamente.
+echo Idioma no reconocido: espa√±ol e ingl√©s √∫nicamente.
 goto fin
 
 :Wingles
 if %silencioso% == 1 goto paso2
-echo Windows en inglÈs.
+echo Windows en ingl√©s.
 :paso2
 set idioma=1
 goto rutina
 
 :Wcastellano
 if %silencioso% == 1 goto paso3
-echo Windows en espaÒol.
+echo Windows en espa√±ol.
 :paso3
 set idioma=2
 
 :rutina
-echo hay-red.cmd [versiÛn 7.0 * junio\2023]
+echo hay-red.cmd [versi√≥n 8.0 * junio\2024]
 echo Comprobando si hay red...
 ping -n 3 1.1.1.1 > nul
 if %ERRORLEVEL% == 0 goto rutina2
 if %idioma% == 2 goto Wcast2A
 
-REM Averigua si el servicio DNS est· activo
+REM Averigua si el servicio DNS est√° activo
 nslookup 1.1.1.1 | findstr /r "Name" > nul
 if %ERRORLEVEL% == 0 goto siga
 goto nRed
 
 :Wcast2A
-REM Averigua si el servicio DNS est· activo
+REM Averigua si el servicio DNS est√° activo
 nslookup 1.1.1.1 | findstr /r "Nombre" > nul
 if %ERRORLEVEL% == 0 goto Wcast2
 goto nRed
@@ -64,13 +64,13 @@ goto nRed
 :rutina2
 echo Aguarde un momento...
 if %idioma% == 2 goto Wcast2
-REM Busca la palabra "unreachable." al final de la lÌnea de textos
+REM Busca la palabra "unreachable." al final de la l√≠nea de textos
 ping -n 3 1.1.1.1|findstr /r "able.\>" > nul
 if %ERRORLEVEL% == 0 goto nRed2
 goto siga
 
 :Wcast2
-REM Busca la palabra "inaccesible." al final de la lÌnea de textos
+REM Busca la palabra "inaccesible." al final de la l√≠nea de textos
 ping -n 3 1.1.1.1|findstr /r "ible.\>" > nul
 if %ERRORLEVEL% == 0 goto nRed2
 
@@ -94,7 +94,7 @@ echo ** Su zona horaria local **
 w32tm /tz
 
 :paso4
-echo --- SÌ. Si hay red %USERNAME%. ---
+echo --- S√≠. Si hay red %USERNAME%. ---
 goto fin
 
 :nRed
@@ -103,7 +103,7 @@ netsh interface show interface
 
 :paso5
 echo No, no hay red.
-echo No hay conexiÛn hacia el exterior.
+echo No hay conexi√≥n hacia el exterior.
 goto fin
 
 :nRed2
@@ -112,7 +112,7 @@ netsh interface show interface
 
 :paso6
 echo No, no hay red.
-echo La tarjeta de red est· desconectada.
+echo La tarjeta de red est√° desconectada.
 
 :fin
 set silencioso=
