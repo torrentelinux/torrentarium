20ae27f2ce6de720b942c02019cf1f56f70fd53a
Torrentarium <torrentelinux@gmail.com>
2024-10-10 21:56:38 -0300

    Add files via upload

--- base/fuente/Java/JavaApplication1.java
+++ base/fuente/Java/JavaApplication1.java
@@ -1,1 +1,33 @@
+// Ciclo infinito
 
+package javaapplication1;
+
+/**
+ *
+ * @author euxeniu
+ */
+public class JavaApplication1 
+{
+        static boolean cycle;
+     
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        do 
+        {
+            print("ciclando...");
+        } while(repeat(cycle));
+    }
+    
+    private static void print(String ciclando) 
+    {
+        System.out.println(ciclando);
+    }
+
+    private static boolean repeat(boolean continuar) 
+    {
+        return (continuar = !continuar);
+    }
+}
