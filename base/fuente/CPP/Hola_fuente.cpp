6eaf02b626a3c83b66c186042fd4e13b4997cc0e
Torrentarium <torrentelinux@gmail.com>
2025-11-08 12:59:22 -0300

    Add files via upload

---
+++ base/fuente/CPP/Hola_fuente.cpp
@@ -0,0 +1,23 @@
+﻿// Programa C++ ¡Hola a todos!
+// construído con macros en español
+
+#include <iostream>
+using namespace std;
+
+#define muestre          cout<<
+#define cierre           <<endl
+#define siguiente_linea  "\n"
+#define programa         main()
+#define inicie           {
+#define termine 	 }
+#define mensaje(t)       #t
+
+programa
+inicie
+   muestre
+     mensaje(¡Hola a todos!)
+     siguiente_linea
+     mensaje(--------------)
+   cierre;
+termine
+
