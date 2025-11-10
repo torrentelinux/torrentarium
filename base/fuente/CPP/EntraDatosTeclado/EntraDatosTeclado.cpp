2b469a1596142451b6409d4b28f7f8ca773abcd5
Torrentarium <torrentelinux@gmail.com>
2025-11-10 13:28:44 -0300

    Add files via upload

---
+++ base/fuente/CPP/EntraDatosTeclado/EntraDatosTeclado.cpp
@@ -0,0 +1,95 @@
+﻿// __________________________________________________________
+// Programa   : EntraDatosTeclado.cpp
+// Descripcion: Dialogando con la computadora en español.
+// Plataforma : Embarcadero C++ Builder, Windows.
+// Licencia   : Software Libre.
+// __________________________________________________________
+#include <iostream>
+#include <tchar.h>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <mem.h>
+
+#include "macros-esp.h"
+
+comentario Declara la tarea 'limpiar' con sus argumentos.
+declare tarea limpiar(region_memoria m, tam_bytes t);
+
+programa
+inicie
+	def_entero pos1 sin_datos_entero;
+	def_entero pos2 sin_datos_entero;
+	string buscar;
+	def_caracteres entrada[96] sin_datos_caracteres;
+
+   comando(chcp 1252>nul);
+
+   muestre mensaje(Dialogando con la máquina.) cierre;
+   muestre mensaje(¿De dónde eres?) cierre;
+
+   muestre mensaje(Responda:) ademas " " ahora;
+   espere.linea_texto(entrada, 80);
+
+   si_es(strlen(entrada) > 0) cierto
+     muestre mensaje(Tú eres) ademas " " ademas entrada cierre;
+   si_no
+     muestre mensaje(No has respondido.) cierre;
+
+   muestre mensaje(¿Vives allí?) cierre;
+
+   limpiar(entrada, 96);
+   muestre mensaje(Responda:) ademas " " ahora;
+   espere.linea_texto(entrada, 80);
+
+   buscar = entrada;
+   pos1 = buscar.find("no");
+   pos2 = buscar.find("si");
+
+   si_es(pos1 != string::npos) cierto
+   inicie
+     si_es(strcmp("no", buscar.substr(pos1, 2).c_str()) == 0) cierto
+       muestre mensaje(Entiendo que quieras privacidad.) cierre;
+   termine
+   si_no
+     inicie
+       muestre mensaje(Me gustaría hacerte una visita...) cierre;
+       si_es(pos2 != string::npos) cierto
+	 muestre mensaje(Mañana podría visitarte.) cierre;
+     termine
+
+   limpiar(entrada, 96);
+   muestre mensaje(Responda:) ademas " " ahora;
+   espere.linea_texto(entrada, 80);
+
+   buscar = entrada;
+   pos1 = buscar.find("no");
+   pos2 = buscar.find("si");
+
+   si_es(pos2 != string::npos) cierto
+     muestre mensaje(Ok. Nos vemos pronto.) cierre;
+   si_no
+     si_es(pos1 != string::npos) cierto
+       muestre mensaje(En otra ocación será...) cierre;
+     si_no
+       inicie
+	 muestre mensaje(Como tú digas. Yo respeto tu decisión.) cierre;
+
+	 si_es(strlen(entrada) == 0) cierto
+	   muestre mensaje(Estás mudo hoy...) cierre;
+       termine
+
+   muestre mensaje(Dialogo terminado.) cierre;
+
+   comentario Efectúa una pausa hasta que se presiona la tecla 'Intro'
+   espere.intro;
+
+   comentario Retorna al S.O. y devuelve 0
+   retorne 0;
+termine
+
+comentario Define la tarea 'limpiar'
+tarea limpiar(region_memoria m, tam_bytes t)
+inicie
+   setmem(m, t, 0);
+termine
---
+++ base/fuente/CPP/EntraDatosTeclado/macros-esp.h
@@ -0,0 +1,34 @@
+﻿// __________________________________________________________
+// Encabezamiento: macros-esp.h
+// Descripcion   : Definiciones de macros en español.
+// Licencia      : Software Libre.
+// __________________________________________________________
+
+#define programa	int _tmain(int argc, _TCHAR* argv[])
+#define inicie           {
+#define termine 	 }
+#define muestre          cout<<
+#define cierre           <<endl
+#define mensaje(t)       #t
+#define ahora		<<flush
+#define ademas          <<
+#define espere          cin
+#define intro           get()
+#define devuelve        return
+#define retorne         return
+#define linea_texto     getline
+#define comando(t)      system(#t)
+#define tarea           void
+#define region_memoria  void*
+#define tam_bytes       unsigned int
+#define declare
+#define comentario	/##/
+#define si_es           if
+#define cierto
+#define si_no           else
+#define def_caracteres  char
+#define sin_datos_caracteres    ={""}
+#define def_entero      int
+#define sin_datos_entero        =0
+
+using namespace std;
